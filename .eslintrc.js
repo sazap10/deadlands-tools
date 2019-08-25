fc8ad2a30d27b7822cd6873cf2e20219085597e9
Sachin Pande <sazap10@gmail.com>
2019-08-25 12:21:09 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:mdx/recommended'
+  ],
+  overrides: [
+    {
+      files: ["*.mdx"],
+      extends: ["plugin:mdx/overrides"]
+    }
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
