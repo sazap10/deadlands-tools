987958843744a7f26c075e835c92680874408b6e
Sachin Pande <sazap10@gmail.com>
2019-08-30 22:07:48 +0100

    configure app and error pages with bugsnag

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,30 @@ import React from 'react'
 import App from 'next/app'
 import Layout from '../components/Layout'
 
+import bugsnagClient from '../lib/bugsnag'
+import Error from './_error'
+
+const ErrorBoundary = bugsnagClient.getPlugin('react')
+
 export default class MyApp extends App {
+  static async getInitialProps ({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
   render () {
     const { Component, pageProps } = this.props
     return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ErrorBoundary FallbackComponent={Error}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ErrorBoundary>
     )
   }
 }
