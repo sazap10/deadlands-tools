4f2ad4c25ad16b48f0182ca70aae3afcba3d797d
Sachin Pande <sazap10@gmail.com>
2020-05-09 15:22:31 +0100

    upgrade v7

--- pages/_error.js
+++ pages/_error.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import ErrorPage from 'next/error'
-import bugsnagClient from '../lib/bugsnag'
-
+import Bugsnag from '@bugsnag/js'
 export default class Page extends React.Component {
   static propTypes = {
     statusCode: PropTypes.number
   };
   static async getInitialProps (ctx) {
-    if (ctx.err) bugsnagClient.notify(ctx.err)
+    if (ctx.err) Bugsnag.notify(ctx.err)
     return ErrorPage.getInitialProps(ctx)
   }
   render () {
